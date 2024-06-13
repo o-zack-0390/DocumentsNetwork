d0cbf44310f09e4d21374b2fedb13b568a568e72
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2024-06-13 13:30:13 +0900

    文章の整形処理を追加.

--- server-mecab/serverMecab.js
+++ server-mecab/serverMecab.js
@@ -2,7 +2,30 @@ import express from 'express';
 import MeCab from 'mecab-async';
 
 /* ==== Begin Function ==== */
-function parseAsync(text)
+function preprocessing(text)
+{
+    /*
+        文章を以下の形式に整形する関数
+        ・ 記号を' 'に置換
+        ・ アルファベットを小文字に統一
+        ・ 数値を 0 に統一
+        ・ 「,」を「、」に置換
+        ・ 「\t」を削除
+    */
+    const punctuation      = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
+    const translationTable = {};
+    for (const char of punctuation) {
+        translationTable[char] = ' ';
+    }
+    text = text.split('').map(char => translationTable[char] || char).join('');
+    text = text.toLowerCase();
+    text = text.replace(/[0-9]+/g, '0');
+    text = text.replace(/,/g, '、');
+    text = text.replace(/\t/g, '');
+    return text;
+}
+
+function parseAsync(text, mecab)
 {
     /* わかち書き文章を生成する関数 */
     return new Promise((resolve, reject) => {
@@ -18,8 +41,8 @@ function parseAsync(text)
 /* ==== End Function ==== */
 
 /* ==== Begin MeCab Setting ==== */
-const mecab   = new MeCab();
 const dicDir  = "/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd";
+const mecab   = new MeCab();
 mecab.command = `mecab -d ${dicDir}`;
 /* ==== End MeCab Setting ==== */
 
@@ -45,7 +68,8 @@ app.use((req, res, next) => {
 app.post('/', async (req, res) => {
     const text = req.body.text;
     try {
-        const words = await parseAsync(text);
+        let words = await preprocessing(text)
+            words = await parseAsync(words, mecab);
         res.json({ wakachiSentence: words.join(' ') });
     } catch (err) {
         console.error(err);
